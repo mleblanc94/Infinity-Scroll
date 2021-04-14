881ab36ba5413858b8ff29a72ed3213d1cab2d8e
mleblanc94 <leblancmichael94@gmail.com>
2021-04-13 21:11:42 -0400

    changes

--- script.js
+++ script.js
@@ -7,19 +7,30 @@ const loader = document.getElementById('loader');
 
 let photosArray = [];
 
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attribute[key]);
+  }
+}
+
 //Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
   //Run function for each object in photosArray
   photosArray.forEach((photo) => {
        //Create <a> to link to Unsplash
        const item = document.createElement('a');
-       item.setAttribute('href', photo.links.html);
-       item.setAttribute('target', '_blank');
+      setAttributes(item, {
+        href: photo.links.html,
+        target: '_blank',
+      });
     // Create <img> for photo
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     // Put <img> inside <a>, then put both inside container element
     item.appendChild(img);
     imageContainer.appendChild(item);
